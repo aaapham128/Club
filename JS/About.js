b1b656a06a42005b4ec3ca17744a185a93221cb5
aaapham128 <40476604+aaapham128@users.noreply.github.com>
2018-07-19 15:57:58 -0700

    Add files via upload

---
+++ JS/About.js
@@ -0,0 +1,6 @@
+alert("This is an alert box.");
+
+var myAbout = document.getElementById("aboutMore").id;
+var mySkills = document.getElementById("learnSkills").id;
+var mySignup = document.getElementById("member").id;
+var myContact document.getElementById("socials").id;
